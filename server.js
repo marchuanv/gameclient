0400abf1ec01ef6b59772ba451c8a35cbe0ceac7
marchuan <marchuanv@gmail.com>
2017-07-06 23:58:39 +0200

    trying to solve a azure deploy problem

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser')
-var common = require('./lib/utils.js');
+var common = require('./release/utils.js');
 
 var app = express();
 var port = process.env.PORT || 3000;
