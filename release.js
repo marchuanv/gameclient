8a69d47d305584791fde6b32e8e26ff12d49c0e4
marchuan <marchuanv@gmail.com>
2017-07-07 02:30:12 +0200

    working on publish

--- release.js
+++ release.js
@@ -1,118 +1,122 @@
 var common = require('./lib/utils.js');
 var path = require('path');
 var beautify = require('js-beautify').js_beautify;
+var UglifyJS = require("uglify-js");
+
 var libDir = __dirname+"/lib";
 var extLib = __dirname+"/external";
 var assets = __dirname+"/assets";
 var releaseDir = __dirname + "/release";
+var publishDir = __dirname + "/publish";
 
+function compress(fileName, jsCode){
+	var result  = UglifyJS.minify(jsCode); // parse code and get the initial AST
+	if (result.error){
+		throw "Tried to compress " + fileName +" but resulted in error: "+ result.error;
+	}else{
+		return result.code // compressed code here
+	}
+};
 
-function createRelease(){
-	console.log();
-	console.log('----------------------< CLEANUP >---------------------');
-	common.enumerateDir(releaseDir, ".js", function (resolvedPath) {
-		console.log("removing ",resolvedPath);
-		common.removeFile(resolvedPath);
-	},function complete(){
-	},function fail(){
-	});
-	common.enumerateDir(releaseDir, ".json", function (resolvedPath) {
-		console.log("removing ",resolvedPath);
-		common.removeFile(resolvedPath);
-	},function complete(){
-	},function fail(){
-	});
-	common.enumerateDir(releaseDir, ".html", function (resolvedPath) {
-		console.log("removing ",resolvedPath);
-		common.removeFile(resolvedPath);
-	},function complete(){
-	},function fail(){
-	});
-
-	console.log();
-	console.log('----------------------< GENERATING JS FILES >---------------------');
-	common.enumerateDir(libDir, ".js", function item (jsResolvedPath) {
-
-		var isSceneTemplate = false;
-		if (jsResolvedPath.indexOf("templates") >= 0 && jsResolvedPath.endsWith("Scene.js") >= 0){
-			isSceneTemplate = true;
-		}
-		if (!isSceneTemplate){
-			common.readFile(jsResolvedPath, function complete(data){ //DO JS FILES
-				var newData = data;
-				var releaseFilePath = jsResolvedPath.replace("lib","release");
-				if (releaseFilePath.indexOf("game") >= 0){
-					var startExp = '/*[';
-					var endExp = ']*/';
-					var startIndex = newData.indexOf('/*[');
-					var startIndexOffset =  startIndex+ startExp.length;
-					var endIndex = newData.indexOf(']*/',startIndex);
-					var endIndexOffset = endIndex + endExp.length-3;
-					var header  = newData.substring(startIndex, endIndex + endExp.length);
-					var arguments = newData.substring(startIndexOffset, endIndexOffset);
-					var factoryDepArray = arguments.split(',');
-					var functionName = common.getFileName(releaseFilePath);
-					functionName = functionName.replace(".js","");
-
-					newData = newData.replace(header,"");
-					arguments = arguments.replace(/\ /g, "");
-
-					var isClass = false;
-					if (newData.indexOf("/*CLASS*/") >= 0){
-						isClass = true;
-						newData = newData.replace("/*CLASS*/","");
-					}
-
-					newData = "factory(function(factory){\r\n factory.register("+isClass+", function " +  functionName + "("+arguments+"){\r\n" + newData + " \r\n }, function error(err){})});";
-					newData = beautify(newData, { indent_size: 2 });
-				} 
-				common.saveFile(releaseFilePath, newData, function () {
-					console.log("code file saved at ",releaseFilePath);
-				}, function fail(err){
-					console.log("ERROR:",err);
-				});
-			}, function fail(){
-			});
-		}
-	},function complete(){ //DO CONFIGURATIONS
-		
+function Release(){
+	this.create = function(){
 		console.log();
-		console.log('----------------------< GENERATING CONFIG >---------------------');
-		
-		common.enumerateDir(libDir, ".json", function (jsonResolvedPath) {
+		console.log('----------------------< CLEANUP >---------------------');
+		common.enumerateDir(releaseDir, ".js", function (resolvedPath) {
+			console.log("removing ",resolvedPath);
+			common.removeFile(resolvedPath);
+		},function complete(){
+		},function fail(){
+		});
+		common.enumerateDir(releaseDir, ".json", function (resolvedPath) {
+			console.log("removing ",resolvedPath);
+			common.removeFile(resolvedPath);
+		},function complete(){
+		},function fail(){
+		});
+		common.enumerateDir(releaseDir, ".html", function (resolvedPath) {
+			console.log("removing ",resolvedPath);
+			common.removeFile(resolvedPath);
+		},function complete(){
+		},function fail(){
+		});
+
+		console.log();
+		console.log('----------------------< GENERATING JS FILES >---------------------');
+		common.enumerateDir(libDir, ".js", function item (jsResolvedPath) {
+
 			var isSceneTemplate = false;
-			if (jsonResolvedPath.indexOf("templates") >= 0 && jsonResolvedPath.endsWith("Scene.json")){
+			if (jsResolvedPath.indexOf("templates") >= 0 && jsResolvedPath.endsWith("Scene.js") >= 0){
 				isSceneTemplate = true;
 			}
 			if (!isSceneTemplate){
-				var releaseFilePath = jsonResolvedPath.replace("lib","release");
-				var functionName = common.getFileName(releaseFilePath);
-				functionName = functionName.replace(".json","Config");
-				releaseFilePath = releaseFilePath.replace(".json","Config.js");
-				common.readFile(jsonResolvedPath, function(jsonStr){
-					var config = "factory(function(factory){ factory.register(false, function " +  functionName + "(){\r\n return `"+jsonStr+"`; \r\n }, function error(err){})});";
-					common.saveFile(releaseFilePath, config, function saved(){
-						console.log("config json file saved at ",releaseFilePath);
-					}, function fail(){
-					});
+				common.readFile(jsResolvedPath, function complete(data){ //DO JS FILES
+					var newData = data;
+					var releaseFilePath = jsResolvedPath.replace("lib","release");
+					if (releaseFilePath.indexOf("game") >= 0){
+						var startExp = '/*[';
+						var endExp = ']*/';
+						var startIndex = newData.indexOf('/*[');
+						var startIndexOffset =  startIndex+ startExp.length;
+						var endIndex = newData.indexOf(']*/',startIndex);
+						var endIndexOffset = endIndex + endExp.length-3;
+						var header  = newData.substring(startIndex, endIndex + endExp.length);
+						var arguments = newData.substring(startIndexOffset, endIndexOffset);
+						var factoryDepArray = arguments.split(',');
+						var functionName = common.getFileName(releaseFilePath);
+						functionName = functionName.replace(".js","");
 
+						newData = newData.replace(header,"");
+						arguments = arguments.replace(/\ /g, "");
+
+						var isClass = false;
+						if (newData.indexOf("/*CLASS*/") >= 0){
+							isClass = true;
+							newData = newData.replace("/*CLASS*/","");
+						}
+
+						newData = "factory(function(factory){\r\n factory.register("+isClass+", function " +  functionName + "("+arguments+"){\r\n" + newData + " \r\n }, function error(err){})});";
+						newData = beautify(newData, { indent_size: 2 });
+					}
+					common.saveFile(releaseFilePath, newData, function () {
+						console.log("code file saved at ",releaseFilePath);
+					}, function fail(err){
+						console.log("ERROR:",err);
+					});
 				}, function fail(){
 				});
 			}
-		},function complete(){
+		},function complete(){ //DO CONFIGURATIONS
+			
 			console.log();
-			console.log('----------------------< GENERATE EXTERNAL LIBRARIES >---------------------');
-			common.enumerateDir(extLib, ".js", function (jsResolvedPath) {
-				var releaseFilePath = jsResolvedPath.replace("external","release");
-				common.readFile(jsResolvedPath, function(jsFileData){
-					common.saveFile(releaseFilePath, jsFileData, function saved(){
-						console.log(releaseFilePath + " was created.");
+			console.log('----------------------< GENERATING CONFIG >---------------------');
+			
+			common.enumerateDir(libDir, ".json", function (jsonResolvedPath) {
+				var isSceneTemplate = false;
+				if (jsonResolvedPath.indexOf("templates") >= 0 && jsonResolvedPath.endsWith("Scene.json")){
+					isSceneTemplate = true;
+				}
+				if (!isSceneTemplate){
+					var releaseFilePath = jsonResolvedPath.replace("lib","release");
+					var functionName = common.getFileName(releaseFilePath);
+					functionName = functionName.replace(".json","Config");
+					releaseFilePath = releaseFilePath.replace(".json","Config.js");
+					common.readFile(jsonResolvedPath, function(jsonStr){
+						var obj = JSON.parse(jsonStr);
+						var _newConfig = JSON.stringify(obj);
+						var config = "factory(function(factory){ factory.register(false, function " +  functionName + "(){\r\n return '"+_newConfig+"'; \r\n }, function error(err){})});";
+						common.saveFile(releaseFilePath, config, function saved(){
+							console.log("config json file saved at ",releaseFilePath);
+						}, function fail(){
+						});
+
 					}, function fail(){
 					});
-				}, function fail(){
-				});
-
-				common.enumerateDir(extLib, ".css", function (jsResolvedPath) {
+				}
+			},function complete(){
+				console.log();
+				console.log('----------------------< GENERATE EXTERNAL LIBRARIES >---------------------');
+				common.enumerateDir(extLib, ".js", function (jsResolvedPath) {
 					var releaseFilePath = jsResolvedPath.replace("external","release");
 					common.readFile(jsResolvedPath, function(jsFileData){
 						common.saveFile(releaseFilePath, jsFileData, function saved(){
@@ -121,43 +125,74 @@ function createRelease(){
 						});
 					}, function fail(){
 					});
-				},function complete(){
-				});
 
-			},function complete(){
-				console.log();
-				console.log('----------------------< GENERATING INDEX.HTML >---------------------');
-				common.enumerateDir(libDir, ".html", function (jsonResolvedPath) {
-					var releaseFilePath = jsonResolvedPath.replace("lib","release");
-					common.readFile(jsonResolvedPath, function(htmlStr){
-						var newHtmlStr = htmlStr;
-						common.enumerateDir(releaseDir, ".js", function (resolvedJSPath) {
-							if (resolvedJSPath.indexOf("server.js") == -1){
-								var relativePath = path.relative("designs", resolvedJSPath);
-								newHtmlStr = newHtmlStr.replace("[script]",'\r\n<script type="text/javascript" src="'+relativePath+'"></script> [script]');
-								console.log("adding js script",resolvedJSPath);
-							}
-						}, function complete(){
-							newHtmlStr = newHtmlStr.replace("[script]","");
-							common.saveFile(releaseFilePath, newHtmlStr, function saved(){
+					common.enumerateDir(extLib, ".css", function (jsResolvedPath) {
+						var releaseFilePath = jsResolvedPath.replace("external","release");
+						common.readFile(jsResolvedPath, function(jsFileData){
+							common.saveFile(releaseFilePath, jsFileData, function saved(){
 								console.log(releaseFilePath + " was created.");
 							}, function fail(){
 							});
-						}, function fail(err){
-							console.log("Error:",err);
+						}, function fail(){
 						});
+					},function complete(){
+					});
+
+				},function complete(){
+					console.log();
+					console.log('----------------------< GENERATING INDEX.HTML >---------------------');
+					common.enumerateDir(libDir, ".html", function (jsonResolvedPath) {
+						var releaseFilePath = jsonResolvedPath.replace("lib","release");
+						common.readFile(jsonResolvedPath, function(htmlStr){
+							var newHtmlStr = htmlStr;
+							common.enumerateDir(releaseDir, ".js", function (resolvedJSPath) {
+								if (resolvedJSPath.indexOf("server.js") == -1){
+									var relativePath = path.relative("designs", resolvedJSPath);
+									newHtmlStr = newHtmlStr.replace("[script]",'\r\n<script type="text/javascript" src="'+relativePath+'"></script> [script]');
+									console.log("adding js script",resolvedJSPath);
+								}
+							}, function complete(){
+								newHtmlStr = newHtmlStr.replace("[script]","");
+								common.saveFile(releaseFilePath, newHtmlStr, function saved(){
+									console.log(releaseFilePath + " was created.");
+								}, function fail(){
+								});
+							}, function fail(err){
+								console.log("Error:",err);
+							});
+						}, function fail(){
+						});
+					},function complete(){
+						
 					}, function fail(){
 					});
-				},function complete(){
-					
 				}, function fail(){
 				});
 			}, function fail(){
 			});
 		}, function fail(){
 		});
-	}, function fail(){
-	});
+	};
+	this.publish = function(cbComplete, cbFail){
+
+		console.log('----------------------< STARTING PUBLISH >---------------------');
+		var compressedJs;
+		common.enumerateDir(releaseDir, ".js", function (jsResolvedPath) {
+			if (jsResolvedPath.indexOf(".min.") == -1){
+				common.readFile(jsResolvedPath, function(jsScript){
+					if (jsScript){
+						compressedJs += compress(jsResolvedPath, jsScript);
+					}
+				}, cbFail);
+			}
+		},function complete(){
+			var releaseFilePath = publishDir + "/game.min.js";
+			common.saveFile(releaseFilePath, compressedJs, function saved(){
+				console.log(releaseFilePath + " was created.");
+				cbComplete();
+			},cbFail);
+		},cbFail);
+	};
 };
 
-module.exports = createRelease();
+module.exports = new Release();
--- server.js
+++ server.js
@@ -1,40 +1,35 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser')
+var release = require("./release.js");
 
 var app = express();
 var port = process.env.PORT || 3000;
 
 var external = path.resolve("/letfly",__dirname+"/external");
 var assets = path.resolve("/letfly",__dirname+"/assets");
-var release = path.resolve("/letfly",__dirname+"/release");
+var releaseDir = path.resolve("/letfly",__dirname+"/release");
 
 app.use(bodyParser.json());
 app.use("/external", express.static(external));
 app.use("/assets", express.static(assets));
-app.use("/release", express.static(release));
+app.use("/release", express.static(releaseDir));
 
-function deleteRoute(url) {
-	for (var i = app.routes.get.length - 1; i >= 0; i--) {
-		var route = app.routes.get[i];
-		if (route.path === "/" + url) {
-			console.log("removed route: ",route);
-		  	app.routes.get.splice(i, 1);
-		}
-	};
-}
-
-app.get('/', function(req, res) {
-	require("./release.js");
-	setTimeout(function(){
-		res.status(200).sendFile(release+"/index.html");
-	},2000);
+app.get(/^\/publish$/igm, function(req, res) {
+	release.publish(function(publishData){
+		res.status(200).send({
+			message: "All good", 
+			publish: publishData
+		});
+	},function fail(err){
+		console.log("ERROR: ",err);
+	});
 });
 
-app.post('/release', function(req, res) {
-	require("./release.js");
+app.get(/^\/$/igm, function(req, res) {
+	release.create();
 	setTimeout(function(){
-		res.status(200).send({message: "All good"});
+		res.status(200).sendFile(releaseDir+"/index.html");
 	},2000);
 });
 
