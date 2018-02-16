7ba67371ef06e485c44839d2af02dacd40c30040
marchuanv <Marchuanv@gmail.com>
2018-02-16 17:25:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ var path = require('path');
 var bodyParser = require('body-parser')
 var app = express();
 app.use(bodyParser.json());
-app.use(express.static("lib"));
-
+app.use(express.static(path.join(__dirname, 'lib')));
 var port = process.env.PORT || 3000;
-
 app.listen(port, function () {
   console.log('listening on port', port);
 });
