6153bb754de2fc98e2304e3d0e10ecfaa6198763
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2018-02-16 17:09:14 +0200

    cleanup

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser')
-var libDir = path.resolve("/lib",__dirname+"/lib");
 
 var app = express();
 var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use("/external", express.static("external"));
-app.use("/downloads", express.static("downloads"));
 app.listen(port, function () {
   console.log('listening on port', port);
 });
