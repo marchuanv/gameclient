1020b79d8397c1af3a10b5783babd430fe5a5682
marchuanv <Marchuanv@gmail.com>
2018-02-20 17:27:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
+var userId=2;
+var systemUserId=2;
 require('messagebus').create(function(messageBus) {
+  messageBus.publish('registerlibrary', systemUserId, "function PhaserGame(phaserGameConfig, sceneManager, sceneSelector, cache){  }");
+  messageBus.publish('PhaserGame', userId, { userId: userId, class:"PhaserGame"});
+  messageBus.subscribe('PhaserGame', systemUserId, function(data) {
+    console.log("PHASER GAME INSTANCE:",data);
+  });
 });
