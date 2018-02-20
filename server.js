38dc385425e540badd5bcf66d9737984cac28a67
marchuanv <Marchuanv@gmail.com>
2018-02-20 17:20:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,6 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser')
-var app = express();
-app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, 'lib')));
-app.use(express.static(path.join(__dirname, 'node_modules/utils/')));
-
-app.get('/game',function(req,res){
-  res.status(200).send({
-    status:'initialised'
-  });
-});
-
-var port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('listening on port', port);
+require('messagebus').create(function(messageBus) {
 });
