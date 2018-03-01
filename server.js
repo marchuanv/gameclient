80ef8a0f9a6ea23244f4a9ba195c288c62fabafc
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2018-03-01 13:14:52 +0200

    fix

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const applicationId='gamedesigner';
 require('messagebus').create(function(messageBus) {
-  
   messageBus.subscribe('PhaserGame', applicationId, function(data) {
     console.log("PHASER GAME INSTANCE:",data);
+  },function done(){
+	  messageBus.publish('registerlibrary', '', { 
+	  	Id: applicationId, 
+	  	javascript: "function PhaserGame(){ this.isStarted=true; }"
+	  },function done(){
+  			messageBus.publish('PhaserGame', applicationId,{ arguments:[] });
+	  });
   });
-  
-  messageBus.publish('PhaserGame', applicationId,{ arguments:[] });
-  
-  messageBus.publish('registerlibrary', '', { 
-  	Id: applicationId, 
-  	javascript: "function PhaserGame(){ this.isStarted=true; }"
-  });
-
 });
