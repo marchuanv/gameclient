1fbd404fe59a36049820eee72bae67b26210e8ff
marchuanv <Marchuanv@gmail.com>
2018-03-01 23:16:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const applicationId='gamedesigner';
 require('messagebus').create(function(messageBus) {
-
   messageBus.publish('registerlibrary', '', { 
   	Id: applicationId, 
   	javascript: "function PhaserGame(){ this.isStarted=true; }"
   });
   messageBus.subscribe('libraryregistered','',function(){
-  	messageBus.subscribe('PhaserGame', applicationId, function(data) {
+	messageBus.subscribe('getPhaserGame', applicationId, function(data) {
 		console.log("PHASER GAME INSTANCE:",data);
 	});
-	messageBus.publish('PhaserGame', applicationId,{ arguments:[] },function done(){
-	});
+	messageBus.public('createPhaserGame', applicationId, { arguments:[]} );
   });
 });
