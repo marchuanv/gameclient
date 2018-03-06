f3addde6bacbe240ad25c1964e9ad001f4e5d17a
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2018-03-06 12:30:20 +0200

    fix

---
+++ install.sh
@@ -0,0 +1,16 @@
+reset
+
+cd ../messagebus/
+git clean -fdx
+npm install
+npm update
+
+cd ../objectfactory/
+git clean -fdx
+npm install
+npm update
+
+cd ../gamedesigner/
+git clean -fdx
+npm install
+npm update
--- start.sh
+++ start.sh
@@ -5,25 +5,16 @@ export thisserveraddress
 export publishaddress
 
 cd ../messagebus/
-git clean -fdx
-npm install
-npm update
 thisserveraddress='localhost:3000'
 publishaddress='localhost:3000'
 npm start &
 
 cd ../objectfactory/
-git clean -fdx
-npm install
-npm update
 thisserveraddress='localhost:4000'
 publishaddress='localhost:3000'
 npm start &
 
 cd ../gamedesigner/
-git clean -fdx
-npm install
-npm update
 thisserveraddress='localhost:5000'
 publishaddress='localhost:3000'
 npm start &
