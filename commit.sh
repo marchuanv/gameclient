37eaa4f3ea25d872514b468457f1a503f610da65
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2018-03-06 12:15:54 +0200

    fix

---
+++ commit.sh
@@ -0,0 +1,23 @@
+cd ../objectfactory/
+git add -A
+git pull origin master
+git commit -m "fix"
+git push origin master
+
+cd ../gamedesigner/
+git pull origin master
+git add -A
+git commit -m "fix"
+git push origin master
+
+cd ../messagebus/
+git pull origin master
+git add -A
+git commit -m "fix"
+git push origin master
+
+cd ../utils/
+git pull origin master
+git add -A
+git commit -m "fix"
+git push origin master
