8a184cd57b3d533feb5ee6acf8b906cc35fc153e
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2018-03-07 14:03:57 +0200

    fix

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const applicationId='gamedesigner';
+
 const sceneObjBuilderConfig =require('./lib/config/sceneObjBuilder.js');
 const spriteBuilderConfig=require('./lib/config/spriteBuilder.js');
 const animationBuilderConfig=require('./lib/config/animationBuilder.js');
@@ -6,6 +7,7 @@ const textBuilderConfig=require('./lib/config/textBuilder.js');
 const imageBuilderConfig=require('./lib/config/imageBuilder.js');
 const shapeBuilderConfig=require('./lib/config/shapeBuilder.js');
 const designerSceneConfig=require('./lib/config/designerScene.js');
+
 const sceneObjBuilder=require('./lib/game/sceneObjBuilder.js');
 const spriteBuilder=require('./lib/game/spriteBuilder.js');
 const animationBuilder=require('./lib/game/animationBuilder.js');
@@ -13,6 +15,7 @@ const textBuilder=require('./lib/game/textBuilder.js');
 const imageBuilder=require('./lib/game/imageBuilder.js');
 const shapeBuilder=require('./lib/game/shapeBuilder.js');
 const timerBuilder=require('./lib/game/timerBuilder.js');
+const designerScene=require('./lib/designer/designerScene.js');
 
 const objects=[
     animationBuilderConfig,
@@ -28,10 +31,22 @@ const objects=[
     spriteBuilder,
     sceneObjBuilderConfig,
     sceneObjBuilder,
-    designerSceneConfig
+    designerSceneConfig,
+    designerScene
 ];
 
 require('messagebus').create(function(messageBus) {
+
+  messageBus.subscribe('libraryregistered', '', function(data){
+    var registeredLib=data.class;
+    if (registeredLib=="DesignerScene"){
+      messageBus.subscribe('getDesignerScene', applicationId, function(data) {
+        console.log("DESIGNER SCENE INSTANCE:", data);
+      });
+      messageBus.publish('createDesignerScene', applicationId, {});
+    }
+  });
+  
   for (var i = objects.length - 1; i >= 0; i--) {
       const func=objects[i];
       messageBus.publish('registerlibrary', '', {
@@ -39,10 +54,5 @@ require('messagebus').create(function(messageBus) {
       	javascript: func
       });
   };
-  messageBus.subscribe('libraryregistered','',function(){
-    messageBus.subscribe('getPhaserGame', applicationId, function(data) {
-      console.log("PHASER GAME INSTANCE:",data);
-    });
-	  messageBus.publish('createPhaserGame', applicationId, {});
-  });
+
 });
