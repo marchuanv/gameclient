cacb6ce885ee1bf21b4fc2282d1479605d818d9a
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2018-03-23 16:17:03 +0200

    fix

--- server.js
+++ server.js
@@ -54,39 +54,8 @@ const objects=[
     timerBuilder
 ];
 
-require('messagebus').create(function(messageBus) {
-
-  messageBus.subscribe('invalidlibrary', '', function(data){
-    console.log("LIBRARY NOT VALID:",data.javascript);
+require('messagebus').create(function(api) {
+  api.getPhaserGame('', function(_phaserGame){
+    console.log('PHASER GAME: ',_phaserGame);
   });
-  messageBus.subscribe('libraryregistered', '', function(data){
-      var registeredLib=data.class;
-      if (registeredLib=="DesignerScene") {
-        messageBus.subscribe('emptyDesignerScene', applicationId, function(data){
-          console.log("EMPTY INSTANCE FOR DesignerScene:",data.class);
-        });
-        messageBus.subscribe('getDesignerScene', applicationId, function(_data) {
-
-
-          console.log("DESIGNER SCENE INSTANCE:", _data);
-        });
-        messageBus.publish('createDesignerScene', applicationId, {});
-      }
-  });
-  for (var i = config.length - 1; i >= 0; i--) {
-      const func=config[i];
-      messageBus.publish('registerlibrary', '', {
-        Id: applicationId, 
-        javascript: func,
-        isClass: false
-      });
-  };
-  for (var i = objects.length - 1; i >= 0; i--) {
-      const func=objects[i];
-      messageBus.publish('registerlibrary', '', {
-        Id: applicationId, 
-        javascript: func,
-        isClass: true
-      });
-  };
 });
